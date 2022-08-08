e01cc5cf0621e6bed110eb2eee3729527e0c23a3
Ira Strubhar <taylor.strubhar@gmail.com>
2022-08-07 21:32:49 -0700

    adjusting further, trying to solve my posts not displaying to the dashboard

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,9 +2,8 @@ const router = require('express').Router();
 const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
 
-// get all posts for homepage
 router.get('/', (req, res) => {
-  console.log('======================');
+  console.log(req.session);
   Post.findAll({
     attributes: [
       'id',
